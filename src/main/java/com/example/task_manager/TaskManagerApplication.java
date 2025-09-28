f03599560879fc259c99a8ff61d177ce7591da03
Santhana Roysten T <roysten152004@gmail.com>
2025-09-28 18:11:25 +0530

    Update TaskManagerApplication.java

--- src/main/java/com/example/task_manager/TaskManagerApplication.java
+++ src/main/java/com/example/task_manager/TaskManagerApplication.java
@@ -3,7 +3,7 @@ package com.example.task_manager;  // Make sure package is correct
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication  // ✅ Correct annotation
+@SpringBootApplication  
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
